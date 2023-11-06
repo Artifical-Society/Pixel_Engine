c651f6087f324a2d82bc08ab833f46a8868091da
Ruihan Zhao <ruihan.ryen.zhao@gmail.com>
2023-11-06 15:05:34 +0800

    cleanup: clean master branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# Pixel_Engine
-A C++ 3D graphic engine, hopefully support OpenCL, OpenGL and Cuda
--- src/utility/decimal.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Ryen Zhao on 26/10/2023.
-//
-
-#ifndef PIXEL_ENGINE_INTEGER_H
-#define PIXEL_ENGINE_INTEGER_H
-
-#endif //PIXEL_ENGINE_INTEGER_H
